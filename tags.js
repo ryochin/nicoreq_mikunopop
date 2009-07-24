0eb0f96dc01da5dc11347b073d4a878ae28a48e6
Ryo Okamoto <ryo@aquahill.net>
2009-07-24 22:26:43 +0900

    updated tags.js.

--- tags.js
+++ tags.js
@@ -2,6 +2,8 @@
 settings["genreTagWhiteList"] = ["",
 	// ˆê”Ê
 	"ƒeƒNƒm",
+	"Instrumental",
+	"ChipTune",
 
 	// ‘å•S‰È‚æ‚è
 	// http://dic.nicovideo.jp/a/vocaloid%E9%96%A2%E9%80%A3%E3%81%AE%E3%82%BF%E3%82%B0%E4%B8%80%E8%A6%A7
